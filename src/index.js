605e4155b38b8311627135ec081332a6d24b37cb
J-boardman <jaydeboardman@outlook.com>
2022-01-18 23:18:42 +1000

    searching may be the problem

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import createAppendChild from "./createAppendChild";
-import determineWeather from "./determineWeather";
 
 const weatherModule = (() => {
     //INIT: API CALL
     const search = async(search='Brisbane') => {
         try{
-            let fetching = fetch(`//api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ff71f5f04b4f0fca53bfdce95b8318ee`,{mode:'cors'});
+            let fetching = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=ff71f5f04b4f0fca53bfdce95b8318ee`,{mode:'cors'});
             
             let response = await fetching.then(response => response.json())
             
@@ -20,9 +19,7 @@ const weatherModule = (() => {
     search();
 
     //EVENT LISTENERS
-    form.addEventListener('submit', (event) => {
-        handleForm(event, form)
-    });
+    form.addEventListener('submit', (event) => {handleForm(event, form)});
 
     //RENDER
     const render = (info) => {
@@ -32,27 +29,23 @@ const weatherModule = (() => {
         Object.keys(info).forEach(key => {
             createAppendChild('div',main,`${info[key]}`,null,`${key}`);
         })
-        console.log(info)
     }
 
     //ADDITIONAL FUNCTIONS
     const handleForm = (event, form) => {
         event.preventDefault();
-        console.clear();
         search(form[0].value);
         form.reset();
     }
 
     const parseInfo = (info) => {
-        let information = {
+        return {
             'location': `${info.name}, ${info.sys.country}`,
             'temperature': parseKelvins(info.main.temp),
             'weather': info.weather[0].description,
             'minMax' : `Min: ${parseKelvins(info.main.temp_min)}  Max: ${parseKelvins(info.main.temp_max)}`,
             'humidity' : `Humidity: ${info.main.humidity}%`,
         }
-        determineWeather(info.weather[0].main, info.weather[0].description);
-        return information
     }
 
     const parseKelvins = (item) => {
